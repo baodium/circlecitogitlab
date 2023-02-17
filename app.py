27f89ee126d8201f72bd0dd42afa25d80d09dae8
Adewale Obadimu <baodium@gmail.com>
2023-02-17 14:16:18 -0600

    Initial commit

--- app.py
+++ app.py
@@ -1,35 +1,39 @@
+import openai
 import os
 
-import openai
-from flask import Flask, redirect, render_template, request, url_for
+from dotenv import load_dotenv
+from flask import Flask, render_template, request
+
+load_dotenv()
+openai.api_key = os.getenv('OPENAI_API_KEY')
 
 app = Flask(__name__)
-openai.api_key = os.getenv("OPENAI_API_KEY")
 
-
-@app.route("/", methods=("GET", "POST"))
-def index():
-    if request.method == "POST":
-        animal = request.form["animal"]
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
-        )
-        return redirect(url_for("index", result=response.choices[0].text))
-
-    result = request.args.get("result")
-    return render_template("index.html", result=result)
-
-
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
+def convert_to_gitlabci(circleci_config):
+    # Set up OpenAI request
+    prompt = f"Convert the following CircleCI configuration to GitlabCI:\n\n{circleci_config}\n\n---\n\n"
+    model = "text-davinci-002"
+    response = openai.Completion.create(
+        engine=model,
+        prompt=prompt,
+        max_tokens=2048,
+        n=1,
+        stop=None,
+        temperature=0.5,
     )
+
+    # Parse response and return GitlabCI configuration
+    gitlabci_config = response.choices[0].text.strip()
+    return gitlabci_config
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        circleci_config = request.form['circleci_config']
+        gitlabci_config = convert_to_gitlabci(circleci_config)
+        return render_template('index.html', gitlabci_config=gitlabci_config)
+    else:
+        return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run()
